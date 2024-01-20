00736c9693ca8dd4cde795a926215ff2960076d5
CanhotoFT <67978603+CanhotoFT@users.noreply.github.com>
2024-01-20 06:16:59 -0400

    add filter

--- dashboards.py
+++ dashboards.py
@@ -12,6 +12,10 @@ st.set_page_config(layout="wide")
 
 df = pd.read_csv("supermarket_sales.csv", sep=";", decimal=",")
 df["Date"] = pd.to_datetime(df["Date"])
-df=df.sort_values(df["Date"])
+df = df.sort_values("Date")
 
 df["Month"] = df["Date"].apply(lambda x: str(x.year) + "-" + str(x.month))
+month = st.sidebar.selectbox("Mês", df["Month"].unique())
+
+df_filtered = df[df["Month"] == month]
+df_filtered
